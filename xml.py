eea79d45e34cd041d8f3c0a9ca7e4b4972926836
DanielIibarra <noe.gallegosib@uanl.edu.mx>
2022-05-25 22:48:38 -0500

    Archivo python con xml

--- xml.py
+++ xml.py
@@ -1,3 +1,12 @@
+import xml.etree.ElementTree as ET
 
+dates = '''
+<person>
+<name>Noe</name>
+<number type = "intl">8112599353</number>
+<email hide = "yes"/>
+</person>
+'''
 
-
+tree  = ET.fromstring(dates)
+print("name:",tree.find('name').text)
